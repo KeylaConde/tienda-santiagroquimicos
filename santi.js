6ad12e932ff087503b2aa375412778447a157cc8
Keyla <keylacondesa@gmail.com>
2022-11-02 09:57:01 -0500

    Add JS file and shopping cart but I have bugs. I need to remove and to add the mobile-menu and desktop-menu with the 'click' event

---
+++ santi.js
@@ -0,0 +1,18 @@
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuHamburger = document.querySelector('.menu');
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuEmail = document.querySelector('.navbar-email');
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
+
+
+menuHamburger.addEventListener('click', toggleMobileMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuCartIcon.addEventListener('click', toggleCarritoAside);
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
